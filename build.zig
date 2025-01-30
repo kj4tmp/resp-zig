a2906d3092c6d07f0c7f470b6aab5d8f1bbbc9ae
kj4tmp@gmail.com <kj4tmp@gmail.com>
2025-01-29 21:18:08 -0800

    first commit

---
+++ build.zig
@@ -0,0 +1,16 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    _ = b.addModule("resp", .{
+        .root_source_file = b.path("src/root.zig"),
+    });
+    const lib_unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_lib_unit_tests = b.addRunArtifact(lib_unit_tests);
+    b.default_step.dependOn(&run_lib_unit_tests.step);
+}
